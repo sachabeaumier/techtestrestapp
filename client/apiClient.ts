afecd3ab605780abfcdfa2c6e74ab586409847c4
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-07-22 02:44:35 +1200

    reveal infra technical test

--- client/apiClient.ts
+++
@@ -1,5 +0,0 @@
-import request from 'superagent'
-
-export function getGreeting(): Promise<string> {
-  return request.get('/greeting').then((res) => res.body.greeting)
-}
--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,34 @@
 import { join } from 'node:path'
 import express from 'express'
-import cors, { CorsOptions } from 'cors'
+// import cors, { CorsOptions } from 'cors'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
-server.use(cors('*' as CorsOptions))
+// server.use(cors('*' as CorsOptions))
 
-server.get('/greeting', (req, res) => {
-  const greetings = ['hola', 'hi', 'hello', 'howdy']
-  const index = Math.floor(Math.random() * greetings.length)
-  console.log(index)
-  res.json({ greeting: greetings[index] })
+server.get('/triangle', (req, res) => {
+  const { a, b, c } = req.query
+  const angle_a = parseInt(a, 10)
+  const angle_b = parseInt(b, 10)
+  const angle_c = parseInt(c, 10)
+
+  if (angle_a + angle_b + angle_c !== 180) {
+    res.status(400)
+    res.json(
+      'error, shape is not a triangle as angles do not equal 180 degrees'
+    )
+  } else if (angle_a == angle_b && angle_b == angle_c) {
+    res.status(200)
+    res.json({ triangle: 'EQUILATERAL' })
+  } else if (angle_a == angle_b || angle_a == angle_c || angle_c == angle_b) {
+    res.status(200)
+    res.json({ triangle: 'ISOSCELES' })
+  } else {
+    res.status(200)
+    res.json({ triangle: 'SCALENE' })
+  }
 })
 
 export default server
